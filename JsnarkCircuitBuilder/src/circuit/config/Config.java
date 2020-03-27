a72ce8ac2a7d72e3b968cae5fe80425d824b255e
Nick Baumann <8076953+nibau@users.noreply.github.com>
2020-03-27 22:04:30 +0100

    jar support

--- JsnarkCircuitBuilder/src/circuit/config/Config.java
+++ JsnarkCircuitBuilder/src/circuit/config/Config.java
@@ -14,25 +14,31 @@ public class Config {
 
 	static Properties properties = new Properties();
 
-	static{
+	static {
 		try {
 			InputStream inStream = new FileInputStream("config.properties");
 			properties.load(inStream);
 		} catch (FileNotFoundException e) {
-			System.err.println("config.properties file not found.");
-			e.printStackTrace();
-			System.exit(0);
+			try {
+				// Load as resource if in jar
+				InputStream inStream = Config.class.getResourceAsStream("/config.properties");
+				properties.load(inStream);
+			} catch (Exception e2) {
+				System.err.println("config.properties file not found.");
+				e2.printStackTrace();
+				System.exit(0);
+			}
 		} catch (IOException e) {
 			System.err.println("config.properties not loaded properly.");
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static final BigInteger FIELD_PRIME = new BigInteger(properties.getProperty("FIELD_PRIME"));
 	public static final int LOG2_FIELD_PRIME = FIELD_PRIME.toString(2).length();
 	public static final String LIBSNARK_EXEC = properties.getProperty("PATH_TO_LIBSNARK_EXEC");
-	
-	public static boolean runningMultiGenerators = properties.getProperty("RUNNING_GENERATORS_IN_PARALLEL").equals("1");	
+
+	public static boolean runningMultiGenerators = properties.getProperty("RUNNING_GENERATORS_IN_PARALLEL").equals("0");
 	public static boolean hexOutputEnabled = properties.getProperty("PRINT_HEX").equals("1");
 	public static boolean outputVerbose = properties.getProperty("OUTPUT_VERBOSE").equals("1");
 	public static boolean debugVerbose = properties.getProperty("DEBUG_VERBOSE").equals("1");
