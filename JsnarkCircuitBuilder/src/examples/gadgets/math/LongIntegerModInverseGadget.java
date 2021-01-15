68d1a70595c8d3723b8dc20f1a77508f23a4a312
Roger Baumgartner <rogerb@student.ethz.ch>
2021-01-14 17:58:34 -0800

    Add a gadget to compute the modular inverse of a LongElement

---
+++ JsnarkCircuitBuilder/src/examples/gadgets/math/LongIntegerModInverseGadget.java
@@ -0,0 +1,77 @@
+package examples.gadgets.math;
+
+import circuit.auxiliary.LongElement;
+import circuit.eval.CircuitEvaluator;
+import circuit.eval.Instruction;
+import circuit.operations.Gadget;
+import circuit.structure.Wire;
+import util.Util;
+
+import java.math.BigInteger;
+
+/**
+ * This gadget computes the modular multiplicative inverse a^(-1) mod m,
+ * where a and m are LongElements.
+ * If restrictRange is set to true, the output will be the sole inverse a^(-1)
+ * for which a < m holds. If restrictRange is false, the inverse may be any
+ * value x for which ax = 1 mod m holds.
+ * It is the responsibility of the caller to ensure that a and m are
+ * relatively co-prime, i.e. the modular inverse actually exists.
+ */
+public class LongIntegerModInverseGadget extends Gadget {
+
+	private final LongElement a; // the value to be inverted
+	private final LongElement m; // the modulus
+	private final boolean restrictRange; // whether to enforce that a^(-1) < m
+	private LongElement inverse;
+
+	public LongIntegerModInverseGadget(LongElement a, LongElement m, boolean restrictRange, String... desc) {
+		super(desc);
+		this.a = a;
+		this.m = m;
+		this.restrictRange = restrictRange;
+		buildCircuit();
+	}
+
+	private void buildCircuit() {
+		Wire[] inverseWires = generator.createProverWitnessWireArray(m.getSize());
+		inverse = new LongElement(inverseWires, m.getCurrentBitwidth());
+		Wire[] quotientWires = generator.createProverWitnessWireArray(m.getSize());
+		LongElement quotient = new LongElement(quotientWires, m.getCurrentBitwidth());
+
+		generator.specifyProverWitnessComputation(new Instruction() {
+			@Override
+			public void evaluate(CircuitEvaluator evaluator) {
+				BigInteger aValue = evaluator.getWireValue(a, LongElement.CHUNK_BITWIDTH);
+				BigInteger mValue = evaluator.getWireValue(m, LongElement.CHUNK_BITWIDTH);
+				BigInteger inverseValue = aValue.modInverse(mValue);
+				BigInteger quotientValue = aValue.multiply(inverseValue).divide(mValue);
+
+				evaluator.setWireValue(inverseWires, Util.split(inverseValue, LongElement.CHUNK_BITWIDTH));
+				evaluator.setWireValue(quotientWires, Util.split(quotientValue, LongElement.CHUNK_BITWIDTH));
+			}
+		});
+
+		inverse.restrictBitwidth();
+		quotient.restrictBitwidth();
+
+		// a * a^(-1) = 1   (mod m)
+		// <=> Exist q:  a * a^(-1) = q * m + 1
+		LongElement product = a.mul(inverse);
+		LongElement oneModM = quotient.mul(m).add(new LongElement(new BigInteger[] {BigInteger.ONE}));
+		product.assertEquality(oneModM);
+
+		if (restrictRange) {
+			inverse.assertLessThan(m);
+		}
+	}
+
+	public LongElement getResult() {
+		return inverse;
+	}
+
+	@Override
+	public Wire[] getOutputWires() {
+		return inverse.getArray();
+	}
+}
--- JsnarkCircuitBuilder/src/zkay/ZkayPaillierFastDecGadget.java
+++ JsnarkCircuitBuilder/src/zkay/ZkayPaillierFastDecGadget.java
@@ -7,6 +7,7 @@ import circuit.operations.Gadget;
 import circuit.structure.Wire;
 import examples.gadgets.math.LongIntegerFloorDivGadget;
 import examples.gadgets.math.LongIntegerModGadget;
+import examples.gadgets.math.LongIntegerModInverseGadget;
 import examples.gadgets.math.LongIntegerModPowGadget;
 import util.Util;
 
@@ -35,7 +36,7 @@ public class ZkayPaillierFastDecGadget extends Gadget {
 
 	private void buildCircuit() {
 		int nSquareMinBits = 2 * nBits - 1; // Minimum bit length of n^2
-		LongElement lambdaInverse = computeInverse(lambda, n, nBits);
+		LongElement lambdaInverse = new LongIntegerModInverseGadget(lambda, n, false, "lambda^(-1)").getResult();
 
 		// plain = L(cipher^lambda mod n^2) / lambda mod n
 		LongElement cPowLambda = new LongIntegerModPowGadget(cipher, lambda, nSquare, nSquareMinBits, "c^lambda").getResult();
@@ -43,27 +44,6 @@ public class ZkayPaillierFastDecGadget extends Gadget {
 		LongElement divByN = new LongIntegerFloorDivGadget(minusOne, n, "(c^lambda - 1) / n").getQuotient();
 		LongElement timesLambdaInverse = divByN.mul(lambdaInverse);
 		plain = new LongIntegerModGadget(timesLambdaInverse, n, nBits, true).getRemainder();
-	}
-
-	private LongElement computeInverse(LongElement value, LongElement mod, int modBits) {
-		Wire[] inverseWires = generator.createProverWitnessWireArray(mod.getSize());
-		LongElement inverse = new LongElement(inverseWires, mod.getCurrentBitwidth());
-
-		generator.specifyProverWitnessComputation(new Instruction() {
-			@Override
-			public void evaluate(CircuitEvaluator evaluator) {
-				BigInteger origValue = evaluator.getWireValue(value, LongElement.CHUNK_BITWIDTH);
-				BigInteger modValue = evaluator.getWireValue(mod, LongElement.CHUNK_BITWIDTH);
-				BigInteger inverseValue = origValue.modInverse(modValue);
-				evaluator.setWireValue(inverseWires, Util.split(inverseValue, LongElement.CHUNK_BITWIDTH));
-			}
-		});
-
-		inverse.restrictBitwidth();
-		LongElement product = value.mul(inverse).align(2 * mod.getSize());
-		LongElement modulo = new LongIntegerModGadget(product, mod, modBits, false).getRemainder();
-		modulo.assertEquality(new LongElement(new BigInteger[] {BigInteger.ONE}));
-		return inverse;
 	}
 
 	private LongElement subtractOneFrom(LongElement value) {
